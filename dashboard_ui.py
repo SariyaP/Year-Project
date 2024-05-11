490c47d333c5c7f9a356b546f85f5f35e2fe4d4d
Sariya Punprang <sariya.pu@ku.th>
2024-05-11 15:22:14 +0700

    add data more story telling

--- dashboard_ui.py
+++ dashboard_ui.py
@@ -24,9 +24,13 @@ class DashBoardUI(tk.Tk):
         self.load_data()
         self.selected = self.data.copy()
         self.df = pd.DataFrame(self.selected.copy())
+        self.all_nutritions = []
         self.init_components()
 
+
     def init_components(self):
+        for name in Nutrition:
+            self.all_nutritions += [name.name]
         self.top_label()
         self.top_cereal()
         self.bar_chart()
@@ -34,15 +38,22 @@ class DashBoardUI(tk.Tk):
         self.scatter_chart()
         self.find_cereal()
 
+
     def top_label(self):
         tk.Canvas(self.window, width=940, height=34, bg='white').place(x=10, y=10)
         tk.Label(self.window, text='Nutritions of cereals', font="Arial 14", bg='white').place(x=15, y=15)
         tk.Label(self.window, text='Manufacture:', font="Arial 14", bg='white').place(x=685, y=15)
         self.mfr = ['All'] + self.get_value_from_attribute('mfr')
-        self.choosen_mfr = ttk.Combobox(self.window, values=self.mfr, state='readonly')
+        self.choosen_mfr = ttk.Combobox(self.window, values=self.mfr, state='readonly', font='Arial 10', width=15)
         self.choosen_mfr.current(0)
-        self.choosen_mfr.bind('<<ComboboxSelected>>', self.choose_mfr)
+        self.choosen_mfr.bind('<<ComboboxSelected>>', self.choose_combobox)
         self.choosen_mfr.place(x=805, y=19)
+        tk.Label(self.window, text='Nutrition:', font="Arial 14", bg='white').place(x=455, y=15)
+        self.choosen_nutrient = ttk.Combobox(self.window, values=self.all_nutritions, state='readonly', font='Arial 10', width=15)
+        self.choosen_nutrient.current(0)
+        self.choosen_nutrient.bind('<<ComboboxSelected>>', self.choose_combobox)
+        self.choosen_nutrient.place(x=535, y=19)
+
 
     def top_cereal(self):
         tk.Canvas(self.window, width=450, height=220, bg='white').place(x=10, y=54)
@@ -69,31 +80,47 @@ class DashBoardUI(tk.Tk):
 
     def num_cereal(self):
         tk.Canvas(self.window, width= 300, height=220, bg='white').place(x= 470, y=54)
-        self.num_all = tk.Label(self.window, text='77', font='Arial 48', bg='white')
+        self.num_all = tk.Label(self.window, text=f'{len(self.df)}', font='Arial 48', bg='white')
         self.num_all.place(x=640, y=104)
         tk.Label(self.window, text='Total Cereal', font='Arial 20', bg='white').place(x=600, y=194)
         tk.Canvas(self.window, width=170, height=105, bg='white').place(x=780, y=54)
-        self.num_hot = tk.Label(self.window, text='3', font='Arial 25', bg='white')
+        self.num_hot = tk.Label(self.window, text=f"{self.df['type'].value_counts().get('Hot', 0)}", font='Arial 25', bg='white')
         self.num_hot.place(x=855, y=74)
         tk.Label(self.window, text='Total Hot Cereal', font='Arial 15', bg='white').place(x=793, y=119)
         tk.Canvas(self.window, width=170, height=105, bg='white').place(x=780, y=169)
-        self.num_cold = tk.Label(self.window, text='74', font='Arial 25', bg='white')
+        self.num_cold = tk.Label(self.window, text=f"{self.df['type'].value_counts().get('Cold', 0)}", font='Arial 25', bg='white')
         self.num_cold.place(x=855, y=184)
         tk.Label(self.window, text='Total Cold Cereal', font='Arial 15', bg='white').place(x=788, y=229)
+        self.n_df = pd.to_numeric(self.df[Nutrition[self.choosen_nutrient.get()].value])
+        self.n_min = tk.Label(self.window, text=f'min: {self.n_df.min()}',
+                         font='Arial 13', bg='white')
+        self.n_min.place(x=480, y=60)
+        self.n_max = tk.Label(self.window, text=f'max: {self.n_df.max()}',
+                         font='Arial 13', bg='white')
+        self.n_max.place(x=480, y=90)
+        self.n_median = tk.Label(self.window, text=f'median: {self.n_df.median()}',
+                            font='Arial 13', bg='white')
+        self.n_median.place(x=480, y=120)
+        self.n_mean = tk.Label(self.window, text=f'mean: {self.n_df.mean():.2f}',
+                            font='Arial 13', bg='white')
+        self.n_mean.place(x=480, y=150)
+        self.n_mode = tk.Label(self.window, text=f'mode: {self.n_df.mode().iloc[0]}',
+                          font='Arial 13', bg='white')
+        self.n_mode.place(x=480, y=180)
+        self.n_sd = tk.Label(self.window, text=f'SD: {self.n_df.std():.2f}',
+                        font='Arial 13', bg='white')
+        self.n_sd.place(x=480, y=210)
+        self.n_cv = tk.Label(self.window, text=f'CV: {self.n_df.std()/self.n_df.mean():.2f}', font='Arial 13', bg='white')
+        self.n_cv.place(x=480, y=240)
+
 
     def bar_chart(self):
         tk.Canvas(self.window, width= 390, height= 246, bg='white').place(x=10, y= 284)
         self.bar_fig, self.bar_ax = plt.subplots(figsize=(3, 2))
-        x = pd.DataFrame(self.selected)
-        fat_data = x['fat'].sort_values()
-        self.bar_ax.bar(fat_data, range(len(fat_data)), color='#41B8D5')
-        self.bar_ax.set_title('Histogram of Fats')
-        self.bar_ax.set_xlabel('Fat')
-        self.bar_ax.set_xlim(-1, 5)
-        if self.choosen_mfr.get() == 'All':
-            self.bar_ax.set_ylim(0, 70)
-        else:
-            self.bar_ax.set_ylim(0, 25)
+        x = self.df
+        data = pd.to_numeric(x[Nutrition[self.choosen_nutrient.get()].value]).sort_values()
+        self.bar_ax.bar(data, range(len(data)), color='#41B8D5')
+        self.bar_ax.set_title(f'Histogram of {self.choosen_nutrient.get()}')
         self.bar_ax.spines[['top', 'right']].set_visible(False)
         self.bar_canvas = FigureCanvasTkAgg(self.bar_fig, master=self.window)
         self.bar_canvas.draw()
@@ -108,12 +135,11 @@ class DashBoardUI(tk.Tk):
     def scatter_chart(self):
         tk.Canvas(self.window, width=390, height=246, bg='white').place(x=410, y=284)
         self.pair_fig, self.pair_ax = plt.subplots(figsize=(3, 2))
-        x = pd.DataFrame(self.selected)
-        sugar = pd.to_numeric(x['sugars'])
+        x = self.df
+        nutrient = pd.to_numeric(x[Nutrition[self.choosen_nutrient.get()].value]).sort_values()
         calorie = x['calories'].sort_values(ascending= False)
-        self.pair_ax.scatter(sugar, calorie, color='#41B8D5')
-        self.pair_ax.set_title('Correlation of Sugars and Calories')
-        self.pair_ax.set_xlim(-4, 20)
+        self.pair_ax.scatter(nutrient, calorie, color='#41B8D5')
+        self.pair_ax.set_title(f'Correlation of {self.choosen_nutrient.get()} and Calories')
         self.pair_ax.spines[['top', 'right']].set_visible(False)
         self.pair_canvas = FigureCanvasTkAgg(self.pair_fig, master=self.window)
         self.pair_canvas.draw()
@@ -145,7 +171,7 @@ class DashBoardUI(tk.Tk):
 
     def get_value_from_attribute(self, attribute):
         values = set()
-        for item in self.data:
+        for item in self.selected:
             values.add(item[attribute])
         return list(values)
 
@@ -156,7 +182,7 @@ class DashBoardUI(tk.Tk):
     def run(self):
         self.window.mainloop()
 
-    def choose_mfr(self, *args):
+    def choose_combobox(self, *args):
         if self.choosen_mfr.get() == "All":
             self.selected = self.data.copy()
             self.df = pd.DataFrame(self.selected)
@@ -173,10 +199,18 @@ class DashBoardUI(tk.Tk):
             self.top_cereal()
 
     def change_label(self):
+        self.n_df = pd.to_numeric(self.df[Nutrition[self.choosen_nutrient.get()].value])
         type = self.df['type'].value_counts()
         self.num_hot['text'] = type.get('Hot', 0)
         self.num_cold['text'] = type.get('Cold', 0)
-        self.num_all['text'] = len(pd.DataFrame(self.selected))
+        self.num_all['text'] = len(self.df)
+        self.n_min['text'] = f'min: {self.n_df.min()}'
+        self.n_max['text'] = f'max: {self.n_df.max()}'
+        self.n_mean['text'] = f'mean: {self.n_df.mean():.2f}'
+        self.n_median['text'] = f'median: {self.n_df.median()}'
+        self.n_mode['text'] = f'mode: {self.n_df.mode().iloc[0]}'
+        self.n_sd['text'] = f'SD: {self.n_df.std():.2f}'
+        self.n_cv['text'] = f'CV: {self.n_df.std()/self.n_df.mean():.2f}'
 
     def filter(self, attribute, value):
         filtered = []
@@ -258,7 +292,7 @@ class FindCereal(tk.Tk):
 
     def more_info(self):
         if self.filtered.curselection() == ():
-            messagebox.showerror('Error Happens', 'Error: Please choose your cereal')
+            messagebox.showerror('Choose your cereal', 'Error: Please choose your cereal')
         else:
             select_cereal = self.listbox[self.filtered.curselection()[0]]
             CerealInformation(select_cereal, self.data)
@@ -270,31 +304,31 @@ class FindCereal(tk.Tk):
 class CerealInformation(tk.Tk):
     def __init__(self, selected_cereal, data):
         super().__init__()
-        self.cereal = self
+        self.window = self
         self.data = data
         self.selected_cereal = selected_cereal
-        self.cereal.title("Cereal Information")
-        self.cereal.minsize(900, 420)
-        self.cereal.maxsize(900, 420)
+        self.window.title("Cereal Information")
+        self.window.minsize(900, 420)
+        self.window.maxsize(900, 420)
         self.init_components()
 
     def init_components(self):
-        tk.Canvas(self.cereal, height=380, width=860, bg='white').place(x=20, y=20)
-        self.name = tk.Label(self.cereal, text=self.selected_cereal, bg='white', font='Arial 30')
+        tk.Canvas(self.window, height=380, width=860, bg='white').place(x=20, y=20)
+        self.name = tk.Label(self.window, text=self.selected_cereal, bg='white', font='Arial 30')
         self.name.place(x=40, y=30)
         self.info = [d for d in self.data if d['name'] == self.selected_cereal]
-        tk.Label(self.cereal, text=f"Rating: {float(self.info[0]['rating']):.2f} out of 100", bg='white', font='Arial 15').place(x=40, y=85)
-        tk.Label(self.cereal, text=f"Manufacture: {self.info[0]['mfr']}", bg='white', font='Arial 14').place(x=40, y=115)
-        tk.Label(self.cereal, text=f"Calorie: {self.info[0]['calories']} per serving", bg='white', font='Arial 13').place(x=40, y=160)
-        tk.Label(self.cereal, text=f"Fat: {self.info[0]['fat']} grams", bg='white', font='Arial 13').place(x=40, y=210)
-        tk.Label(self.cereal, text=f"Fiber: {self.info[0]['fiber']} grams", bg='white', font='Arial 13').place(x=40, y=260)
-        tk.Label(self.cereal, text=f"Carbohydrates: {self.info[0]['carbo']} grams", bg='white', font='Arial 13').place(x=40, y=310)
-        tk.Label(self.cereal, text=f"Potassium: {self.info[0]['potass']} milligrams", bg='white', font='Arial 13').place(x=40, y=360)
-        tk.Label(self.cereal, text=f"Type: {self.info[0]['type']} Cereal", bg='white', font='Arial 13').place(x=270, y=160)
-        tk.Label(self.cereal, text=f"Protein: {self.info[0]['protein']} grams", bg='white', font='Arial 13').place(x=270, y=210)
-        tk.Label(self.cereal, text=f"Sodium: {self.info[0]['sodium']} milligrams", bg='white', font='Arial 13').place(x=270, y=260)
-        tk.Label(self.cereal, text=f"Sugars: {self.info[0]['sugars']} grams", bg='white', font='Arial 13').place(x=270, y=310)
-        tk.Label(self.cereal, text=f"Vitamins: {self.info[0]['vitamins']}", bg='white', font='Arial 13').place(x=270, y=360)
+        tk.Label(self.window, text=f"Rating: {float(self.info[0]['rating']):.2f} out of 100", bg='white', font='Arial 15').place(x=40, y=85)
+        tk.Label(self.window, text=f"Manufacture: {self.info[0]['mfr']}", bg='white', font='Arial 14').place(x=40, y=115)
+        tk.Label(self.window, text=f"Calorie: {self.info[0]['calories']} per serving", bg='white', font='Arial 13').place(x=40, y=160)
+        tk.Label(self.window, text=f"Fat: {self.info[0]['fat']} grams", bg='white', font='Arial 13').place(x=40, y=210)
+        tk.Label(self.window, text=f"Fiber: {self.info[0]['fiber']} grams", bg='white', font='Arial 13').place(x=40, y=260)
+        tk.Label(self.window, text=f"Carbohydrates: {self.info[0]['carbo']} grams", bg='white', font='Arial 13').place(x=40, y=310)
+        tk.Label(self.window, text=f"Potassium: {self.info[0]['potass']} milligrams", bg='white', font='Arial 13').place(x=40, y=360)
+        tk.Label(self.window, text=f"Type: {self.info[0]['type']} Cereal", bg='white', font='Arial 13').place(x=270, y=160)
+        tk.Label(self.window, text=f"Protein: {self.info[0]['protein']} grams", bg='white', font='Arial 13').place(x=270, y=210)
+        tk.Label(self.window, text=f"Sodium: {self.info[0]['sodium']} milligrams", bg='white', font='Arial 13').place(x=270, y=260)
+        tk.Label(self.window, text=f"Sugars: {self.info[0]['sugars']} grams", bg='white', font='Arial 13').place(x=270, y=310)
+        tk.Label(self.window, text=f"Vitamins: {self.info[0]['vitamins']}", bg='white', font='Arial 13').place(x=270, y=360)
         self.plot_pie()
 
     def plot_pie(self):
@@ -309,7 +343,7 @@ class CerealInformation(tk.Tk):
         value_list[2] = float(value_list[2])/1000
         value_list[5] = float(value_list[2]) / 1000
         self.pie_ax.pie(np.array(value_list), labels=all_att, colors=colors)
-        self.pie_canvas = FigureCanvasTkAgg(self.pie_fig, master=self.cereal)
+        self.pie_canvas = FigureCanvasTkAgg(self.pie_fig, master=self.window)
         self.pie_canvas.draw()
         self.pie_canvas_widget = self.pie_canvas.get_tk_widget()
         self.pie_canvas_widget.place(x=445, y=100)
